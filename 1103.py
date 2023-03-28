2a83ac7b8623909d2bb54d5b547887ac2ec890ee
William de Sousa Costa <37248765+WIlliamdeSousa@users.noreply.github.com>
2023-03-28 19:21:53 +0000

    terminou lista 3

--- 1103.py
+++ 1103.py
@@ -1,11 +1,12 @@
 idade = int(input())
-jogo = str(input())
-limite = {'azar': 21, 'mmorpg': 14, 'moba': 10, 'casual': 0}
-if 0 <= idade <= 130:
+if not 0 <= idade <= 130:
     print('Idade invalida.')
-elif jogo not in limite.keys():
-    print('Jogo invalido.')
-elif idade >= limite[jogo]:
-    print('Pode entrar!')
 else:
-    print('Volte daqui há alguns anos.')
+    jogo = str(input())
+    limite = {'azar': 21, 'mmorpg': 14, 'moba': 10, 'casual': 0}
+    if jogo not in limite.keys():
+        print('Jogo invalido.')
+    elif idade >= limite[jogo]:
+        print('Pode entrar!')
+    else:
+        print('Volte daqui há alguns anos.')
