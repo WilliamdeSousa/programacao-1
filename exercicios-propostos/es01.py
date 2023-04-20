fa09b897c59e9ba149da26e13677b4b4b6762d27
William de Sousa Costa <37248765+WIlliamdeSousa@users.noreply.github.com>
2023-04-20 09:19:34 -0300

    Update es01.py

--- exercicios-propostos/es01.py
+++ exercicios-propostos/es01.py
@@ -1,5 +1,6 @@
 s = c = 0
 while (n:=int(input('informe um número: (0 para parar) '))) != 0:
-    s += n
-    c += 1
+    if s > 0:
+        s += n
+        c += 1
 print(f'a média dos números vale {s/c}')
